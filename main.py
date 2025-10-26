9113203b7d22ec00478f5de0a11f616e7ed54616
KajalMishra-29 <kajalmishra0829@gmail.com>
2025-10-26 19:18:23 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from Kidney_Disease_Classification import logger
+from Kidney_Disease_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
 
-logger.info("welcome to our custom log")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = DataIngestionTrainingPipline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<< \n\nx===================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
